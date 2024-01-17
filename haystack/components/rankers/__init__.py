5d515a2082ded071568d4b3b6f3e3060abf18f66
awinml <97467100+awinml@users.noreply.github.com>
2024-01-17 22:54:16 +0530

    Remove DiversityRanker for now

--- haystack/components/rankers/__init__.py
+++ haystack/components/rankers/__init__.py
@@ -1,6 +1,5 @@
 from haystack.components.rankers.meta_field import MetaFieldRanker
 from haystack.components.rankers.transformers_similarity import TransformersSimilarityRanker
 from haystack.components.rankers.lost_in_the_middle import LostInTheMiddleRanker
-from haystack.components.rankers.diversity import DiversityRanker
 
-__all__ = ["MetaFieldRanker", "TransformersSimilarityRanker", "LostInTheMiddleRanker", "DiversityRanker"]
+__all__ = ["MetaFieldRanker", "TransformersSimilarityRanker", "LostInTheMiddleRanker"]
