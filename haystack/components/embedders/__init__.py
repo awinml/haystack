3da7186ba74d4b5ed9205ffc8bf0a8816987ba69
awinml <97467100+awinml@users.noreply.github.com>
2024-01-17 20:58:53 +0530

    Update embedders import temp fix

--- haystack/components/embedders/__init__.py
+++ haystack/components/embedders/__init__.py
@@ -2,18 +2,10 @@ from haystack.components.embedders.hugging_face_tei_text_embedder import Hugging
 from haystack.components.embedders.hugging_face_tei_document_embedder import HuggingFaceTEIDocumentEmbedder
 from haystack.components.embedders.sentence_transformers_text_embedder import SentenceTransformersTextEmbedder
 from haystack.components.embedders.sentence_transformers_document_embedder import SentenceTransformersDocumentEmbedder
-from haystack.components.embedders.openai_document_embedder import OpenAIDocumentEmbedder
-from haystack.components.embedders.openai_text_embedder import OpenAITextEmbedder
-from haystack.components.embedders.azure_text_embedder import AzureOpenAITextEmbedder
-from haystack.components.embedders.azure_document_embedder import AzureOpenAIDocumentEmbedder
 
 __all__ = [
     "HuggingFaceTEITextEmbedder",
     "HuggingFaceTEIDocumentEmbedder",
     "SentenceTransformersTextEmbedder",
     "SentenceTransformersDocumentEmbedder",
-    "OpenAITextEmbedder",
-    "OpenAIDocumentEmbedder",
-    "AzureOpenAITextEmbedder",
-    "AzureOpenAIDocumentEmbedder",
 ]
